e5ce9090f8354d863c02b67f06d75d432ac566a9
Flavia Reis Valverde <flavia@amixsi.com.br>
2015-11-26 11:56:38 -0200

    Criação da conf do git e profile, início da custom shell

--- dotfile.sh
+++ dotfile.sh
@@ -1,6 +1,30 @@
 #!/bin/bash
 
-cd $HOME/dotfiles
+DIR_CONFIG=$HOME/dotfiles
 
-cp vim/.vimrc $HOME/.
-cp vim/vundles.vim $HOME/.vim
+echo "Configurações de Setup - Profile"
+
+if [ -f "$HOME/.profile" ]
+then
+    FILE_PROFILE=$HOME/.profile
+elif [ -f "$HOME/.bash_profile" ]
+then
+    FILE_PROFILE=$HOME/.bash_profile
+fi
+
+if [ -f $FILE_PROFILE ]
+then
+    echo "achou" #echo <<EOT [ -f "$DIR_CONFIG/profile" ] && . "$DIR_CONFIG/profile" EOT >> $FILE_PROFILE
+else
+    echo "Profile não encontrado"
+
+echo "Configurações VIM"
+
+cp $DIR_CONFIG/vim/.vimrc $HOME/.
+cp $DIR_CONFIG/vim/vundles.vim $HOME/.vim
+
+echo "Configurações GIT"
+
+cp $DIR_CONFIG/git/.gitconfig $HOME/.
+
+echo "Done"
